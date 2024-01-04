e2d98c851b360d6157a635ce41be12b13e17ea51
James Fryman <james@fryman.io>
2024-01-04 00:44:32 -0600

    Refactor to working
    While attempting to get this script working, it would not work on my Steam Deck. I began
    to make adjustments to the script to get it back in working order, and made a few changes
    that you can take or leave.

--- Install.sh
+++ Install.sh
@@ -3,6 +3,9 @@
 # Author: Tritschla
 # 2023-04
 
+# Contributors: jfryman
+# 2024-01 - Refactored create_shortcut function to be more generic
+
 # This script does the following:
 # 1. It is creating Resilio4SteamDeck (R4SD) environment
 # 2. It is creating Resilio (rslsync) environment
@@ -13,58 +16,67 @@
 # 7. It is providing an Resilio4SteamRepair Script to reinstall needed packages, lost on SteamDeck OS updates
 # 8. It is providing an Uninstaller for R4SD incl. Resilio
 
+R4SD_DIR="$HOME/.R4SD"
+REPO_URL="https://raw.githubusercontent.com/Tritschla/Resilio4SteamDeck/main"
+RSLSYNC_DIR="$HOME/rslsync"
 
-# Create a hidden directory for R4SD, if not present
-mkdir -p "$HOME/.R4SD" &>/dev/null
-cd "$HOME/.R4SD" || exit 1
+create_shortcut() {
+    local name="$0"
+    local comment="$1"
+    local exec_command="$2"
+    local icon="btsync-gui"
+    local file_location="$3"
 
-# Create a folder for rslsync, if not present
-mkdir -p "$HOME/rslsync" &>/dev/null
-cd "$HOME/rslsync" || exit 1
+    # Remove existing file if it exists
+    rm -rf "$file_location" 1>/dev/null
 
+    # Create a new file with specified properties
+    cat > "$file_location" << EOF
+[Desktop Entry]
+Name=$name
+Comment=$comment
+Exec=bash "$exec_command"
+Icon=$icon
+Terminal=false
+Type=Application
+StartupNotify=false
+EOF
 
-# Check for sudo access
-set -e
+    # Set executable permissions for the file
+    chmod +x "$file_location"
+}
 
-if ! PASSWORD=$(zenity --password --title "Password required" --width=300 2> /dev/null); then
+# Create the R4SD directory if it doesn't exist
+mkdir -p "$R4SD_DIR" &>/dev/null
+
+# Create the rslsync directory if it doesn't exist
+mkdir -p "$RSLSYNC_DIR" &>/dev/null
+
+# Prompt for sudo password
+PASSWORD=$(zenity --password --title "Password required" --width=300 2> /dev/null)
+if [[ -z $PASSWORD ]]; then
     zenity --error --text="Please enter your password." --width=300 2> /dev/null
     exit 1
 fi
 
-echo "$PASSWORD" | sudo -Sv || (zenity --error --text="Incorrect password." --width=300 2> /dev/null && exit 1)
-
-
 # Deactivate readonly filesystem
 echo "$PASSWORD" | sudo -S steamos-readonly disable
-
 
 # Initialize Arch Linux Keys
 echo "$PASSWORD" | sudo -S pacman-key --init
 echo "$PASSWORD" | sudo pacman -Sy archlinux-keyring --noconfirm
 echo "$PASSWORD" | sudo pacman -Syy --noconfirm
 
-
-
 # Install needed package
 echo "$PASSWORD" | sudo -S pacman -Syyu --noconfirm lib32-libxcrypt-compat
 
-
-# Download the rslsync package from AUR
-cd "$HOME" || exit 1
-git clone https://aur.archlinux.org/rslsync.git
-
-
 # Download i386 rslsync package:
-cd "$HOME/.R4SD" || exit 1
+cd "$R4SD_DIR" || exit 1
 wget https://download-cdn.resilio.com/stable/linux-i386/resilio-sync_i386.tar.gz
-
-
-# Extract i386 files to destination folder
-tar -xzf resilio-sync_i386.tar.gz -C "$HOME/rslsync"
-
+tar -xzf resilio-sync_i386.tar.gz -C "$RSLSYNC_DIR"
 
 # Create Resilio rslsync_user.service
-cat <<EOF >| "$HOME/.config/systemd/user/rslsync_user.service"
+cat <<EOF > "$HOME/.config/systemd/user/rslsync_user.service"
 [Unit]
 Description=Resilio Sync per-user service
 After=network.target
@@ -79,74 +91,40 @@ WantedBy=default.target
 
 EOF
 
-
 # Enable and start rslsync_user service
 systemctl --user enable rslsync_user
 systemctl --user start rslsync_user
 
+# Download RepairR4SD and Uninstall-R4SD scripts
+SCRIPTS=("RepairR4SD.sh" "UninstallR4SD.sh")
+for script in "${SCRIPTS[@]}"; do
+    cd "$R4SD_DIR" || exit 1
+    wget "$REPO_URL/$script"
+    chmod +x "$R4SD_DIR/$script"
+done
 
-# Download RepairR4SD
-cd "$HOME/.R4SD" || exit 1
-wget https://raw.githubusercontent.com/Tritschla/Resilio4SteamDeck/main/RepairR4SD.sh
-chmod +x "$HOME/.R4SD/RepairR4SD.sh"
+# Create desktop shortcuts
+create_shortcut "Repair R4SD" \
+    "This tool updates all necessary tools & packages to get Resilio Sync working after an update to SteamOS" \
+    "$HOME/.R4SD/RepairR4SD.sh" \
+    "$HOME/Desktop/RepairR4SD.desktop"
+create_shortcut "Uninstall R4SD" \
+    "This tool uninstalls the R4SD tools and packages" \
+    "$HOME/.R4SD/UninstallR4SD.sh" \
+    "$HOME/Desktop/UninstallR4SD.desktop"
 
+# Create start menu shortcuts
+create_shortcut "Repair R4SD" \
+    "This tool updates all necessary tools & packages to get Resilio Sync working after an update to SteamOS" \
+    "$HOME/.R4SD/RepairR4SD.sh" \
+    "$HOME/.local/share/applications/RepairR4SD.desktop"
 
-# Download of Uninstall-R4SD
-cd "$HOME/.R4SD" || exit 1
-wget https://raw.githubusercontent.com/Tritschla/Resilio4SteamDeck/main/UninstallR4SD.sh
-chmod +x "$HOME/.R4SD/UninstallR4SD.sh"
-
-# Creation of Desktop Shortcuts
-rm -rf "$HOME"/Desktop/RepairR4SD.desktop 2>/dev/null
-echo "#!/usr/bin/env xdg-open
-[Desktop Entry]
-Name=Repair R4SD
-Comment=This tool updates all necessary tools & packages to get Resilio Sync working after a update to SteamOS
-Exec=bash $HOME/.R4SD/RepairR4SD.sh
-Icon=btsync-gui
-Terminal=false
-Type=Application
-StartupNotify=false" >"$HOME"/Desktop/RepairR4SD.desktop
-chmod +x "$HOME"/Desktop/RepairR4SD.desktop
-
-rm -rf "$HOME"/Desktop/UninstallR4SD.desktop 2>/dev/null
-echo "#!/usr/bin/env xdg-open
-[Desktop Entry]
-Name=Uninstall R4SD
-Comment=This tool updates all necessary tools & packages to get Resilio Sync working after a update to SteamOS
-Exec=bash $HOME/.R4SD/UninstallR4SD.sh
-Icon=btsync-gui
-Terminal=false
-Type=Application
-StartupNotify=false" >"$HOME"/Desktop/UninstallR4SD.desktop
-chmod +x "$HOME"/Desktop/UninstallR4SD.desktop
-
-
-# Creation of Start Menu entries
-rm -rf "$HOME"/.local/share/applications/RepairR4SD.desktop 2>/dev/null
-echo "#!/usr/bin/env xdg-open
-[Desktop Entry]
-Name=Repair R4SD
-Exec=bash $HOME/.R4SD/RepairR4SD.sh
-Icon=btsync-gui
-Terminal=false
-Type=Application
-Categories=Utility
-StartupNotify=false" >"$HOME"/.local/share/applications/RepairR4SD.desktop
-chmod +x "$HOME"/.local/share/applications/RepairR4SD.desktop
-
-rm -rf "$HOME"/.local/share/applications/UninstallR4SD.desktop 2>/dev/null
-echo "#!/usr/bin/env xdg-open
-[Desktop Entry]
-Name=Uninstall R4SD
-Exec=bash $HOME/.R4SD/RepairR4SD.sh
-Icon=btsync-gui
-Terminal=false
-Type=Application
-Categories=Utility
-StartupNotify=false" >"$HOME"/.local/share/applications/UninstallR4SD.desktop
-chmod +x "$HOME"/.local/share/applications/UninstallR4SD.desktop
-
+create_shortcut "Uninstall R4SD" \
+    "This tool uninstalls the R4SD tools and packages" \
+    "$HOME/.R4SD/UninstallR4SD.sh" \
+    "$HOME/.local/share/applications/UninstallR4SD.desktop"
 
 # Completion of the installation
-zenity --info --text="R4SD / Resilio has been successfully installed and can be accessed through a browser (e.g. Google Chrome) using the following link: http://localhost:8888" --width=300 2> /dev/null
+zenity --info \
+    --text="R4SD / Resilio has been successfully installed and can be accessed through a browser (e.g. Google Chrome) using the following link: http://localhost:8888" \
+    --width=300 2> /dev/null
--- UninstallR4SD.sh
+++ UninstallR4SD.sh
@@ -1,32 +1,35 @@
 #!/bin/bash
 
 # Author: Tritschla
-# 2023-04
+# Date: 2023-04
 
-# Ask the user if they want to uninstall R4SD
-if ! zenity --question --title="Uninstall R4SD?" --text="Are you sure you want to uninstall Resilio for Steam Deck?" --width=300 2> /dev/null ; then
+# Prompt the user for uninstallation confirmation
+if ! zenity --question --title="Uninstall R4SD?" --text="Are you sure you want to uninstall Resilio for Steam Deck?" --width=300 2> /dev/null; then
     exit 0
 fi
 
-# Stop and disable rslsync user
-systemctl --user daemon-reload
+# Stop and disable rslsync_user service
 systemctl --user stop rslsync_user
 systemctl --user disable rslsync_user
 
-# Delete rslsync_user.service file
+# Remove rslsync_user.service file
 rm -f "$HOME/.config/systemd/user/rslsync_user.service"
+systemctl --user daemon-reload
 
-# Recursively delete rslsync & R4SD folder
+# Remove rslsync and R4SD folders
 rm -rf "$HOME/rslsync"
 rm -rf "$HOME/.R4SD"
 
-# Delete Desktop Icons
-rm -rf "$HOME/Desktop/RepairR4SD.desktop"
-rm -rf "$HOME/Desktop/UninstallR4SD.desktop"
+# Remove desktop icons
+rm -f "$HOME/Desktop/RepairR4SD.desktop"
+rm -f "$HOME/Desktop/UninstallR4SD.desktop"
 
-# Delete Start Menue entries
-rm -rf "$HOME"/.local/share/applications/RepairR4SD.desktop
-rm -rf "$HOME"/.local/share/applications/UninstallR4SD.desktop
+# Remove start menu entries
+rm -f "$HOME/.local/share/applications/RepairR4SD.desktop"
+rm -f "$HOME/.local/share/applications/UninstallR4SD.desktop"
 
-# Notify the user that the uninstallation is complete
-zenity --info --title="Uninstall R4SD" --text="Resilio for Steam Deck has been successfully uninstalled." --width=300 2> /dev/null
+# Notify the user about successful uninstallation
+zenity --info \
+    --title="Uninstall R4SD" \
+    --text="Resilio for Steam Deck has been successfully uninstalled." \
+    --width=300 2> /dev/null
